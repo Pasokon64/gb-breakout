a5405098c6dc6a76ffa7f08e5242efe8ac517d2d
Manoel Almeida <tmanoel64x@gmail.com>
2020-01-14 20:51:55 -0300

    Paddle update

--- main.c
+++ main.c
@@ -1,6 +1,49 @@
-#include <gb/gb.h>
-#include <stdio.h>
+#include "main.h"
 
-void main() {
-    printf("Ola mundo");
+const int SCREEN_WIDTH = 160;
+const int SCREEN_HEIGHT = 144;
+
+int gameSpeed = 0;
+
+void main()
+{
+    /* Setup player */
+    set_sprite_data(0, 3, TilePlayer);
+    set_sprite_tile(0, 0);
+    set_sprite_tile(1, 1);
+    set_sprite_tile(2, 2);
+
+    move_sprite(0, SCREEN_WIDTH / 2 - 8, SCREEN_HEIGHT + 8);
+    move_sprite(1, SCREEN_WIDTH / 2, SCREEN_HEIGHT + 8);
+    move_sprite(2, SCREEN_WIDTH / 2 + 8, SCREEN_HEIGHT + 8);
+
+    SHOW_SPRITES;
+
+    while (true)
+    {
+        keyboard();
+        delay(10 - gameSpeed);
+    }
+}
+
+void keyboard()
+{
+    switch (joypad())
+    {
+    case J_LEFT:
+        movePlayer(-1);
+        break;
+    case J_RIGHT:
+        movePlayer(1);
+        break;
+    default:
+        break;
+    }
+}
+
+void movePlayer(int x)
+{
+    scroll_sprite(0, x, 0);
+    scroll_sprite(1, x, 0);
+    scroll_sprite(2, x, 0);
 }
---
+++ main.h
@@ -0,0 +1,9 @@
+#include <gb/gb.h>
+#include <stdio.h>
+#include "tiles/TilePlayer.c"
+
+#define true 1
+#define false 0
+
+void keyboard();
+void movePlayer(int);
